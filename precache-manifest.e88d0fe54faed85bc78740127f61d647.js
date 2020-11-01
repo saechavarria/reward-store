ff1d704abf39545c5f4acac713ee5bbc82c8b5c3
santiago echavarria rodriguez <sechavarria26@misena.edu.co>
2020-11-01 18:59:51 -0500

    Updates

---
+++ precache-manifest.e88d0fe54faed85bc78740127f61d647.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104abd1e231caf91880f24f669820696",
+    "url": "/reward-store/index.html"
+  },
+  {
+    "revision": "82a8de83f3a6e2ff905a",
+    "url": "/reward-store/static/css/2.efbef625.chunk.css"
+  },
+  {
+    "revision": "82a8de83f3a6e2ff905a",
+    "url": "/reward-store/static/js/2.c8d652de.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/reward-store/static/js/2.c8d652de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c722534794c7c9daff",
+    "url": "/reward-store/static/js/main.1b775d01.chunk.js"
+  },
+  {
+    "revision": "941b31aa1efa18ac6b48",
+    "url": "/reward-store/static/js/runtime-main.b19dc24d.js"
+  },
+  {
+    "revision": "ee9c1c4bc9843e835dec186425359846",
+    "url": "/reward-store/static/media/loading.ee9c1c4b.gif"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/reward-store/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/reward-store/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/reward-store/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/reward-store/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/reward-store/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/reward-store/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/reward-store/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/reward-store/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/reward-store/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/reward-store/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/reward-store/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/reward-store/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/reward-store/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/reward-store/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/reward-store/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/reward-store/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
